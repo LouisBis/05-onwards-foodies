d68b3e14639f0299534b8a415cdee1f511068460
Louis Marais <louis-marais@hotmail.fr>
2024-09-04 09:51:25 +0200

    Add meals grid

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,10 +1,22 @@
 import Link from "next/link";
-
+import styles from "./page.module.css";
+import MealsGrid from "@/components/meals/meals-grid";
 export default function MealsPage() {
   return (
-    <main>
-      <h1>Meals</h1>
-      <Link href="/meals/test">Link</Link>
-    </main>
+    <>
+      <header className={styles.header}>
+        <h1>
+          Delicious meals, created{" "}
+          <span className={styles.highlight}>by you</span>
+        </h1>
+        <p>Choose your favorite recipe and cook it yourself</p>
+        <p className={styles.cta}>
+          <Link href="/meals/share">Share Your Favorite Recipe</Link>
+        </p>
+      </header>
+      <main className={styles.main}>
+        <MealsGrid meals={[]} />
+      </main>
+    </>
   );
 }
--- next.config.js
+++ next.config.js
@@ -1,7 +1,2 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "export", // <=== enables static exports
-  reactStrictMode: true,
-};
-
-module.exports = nextConfig;
+const nextConfig = {};
