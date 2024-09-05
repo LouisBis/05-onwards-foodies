4d626045f0f1032d5fc99e401d83bac8d1e32e2b
Louis Marais <louis-marais@hotmail.fr>
2024-09-05 15:15:51 +0200

    Move server action

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,22 +1,10 @@
+"use client";
+
 import ImagePicker from "@/components/meals/image-picker";
+import { shareMeal } from "@/lib/action";
 import styles from "./page.module.css";
 
 export default function ShareMealPage() {
-  async function shareMeal(formData) {
-    "use server";
-
-    const meal = {
-      title: formData.get("title"),
-      summary: formData.get("summary"),
-      instructions: formData.get("instructions"),
-      image: formData.get("image"),
-      creator: formData.get("name"),
-      creator_email: formData.get("email"),
-    };
-
-    console.log(meal);
-  }
-
   return (
     <>
       <header className={styles.header}>
